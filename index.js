32911331c8bc329777be05d4c1470a41073a38a6
NastifyMe <suprunar2010@gmail.com>
2025-05-14 09:57:59 +0300

    yugu

---
+++ index.js
@@ -0,0 +1,12 @@
+// Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок
+//  (в рядку будуть тільки слова і прогалини)
+//  і повертає найдовше слово в цьому рядку.
+
+const maxWord = prompt('Введіть слова')
+function findLongestWord(string){
+    maxWord.split(' ')
+    for (let i = 0; i < maxWord; i++){
+        maxWord.length > maxWord[i]
+    }
+}
+console.log(maxWord)
